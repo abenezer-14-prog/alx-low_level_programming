7b6a0ff5eab0e2f0be1f68b1721c3ae5398eb325
root <abenezerlulseged@gmail.com>
2022-03-17 11:02:07 -0700

    debugging first time

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,4 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
  * main - tests function that prints if integer is positive or negative
  * Return: 0
@@ -14,3 +13,4 @@ positive_or_negative(i);
 
 return (0);
 }
+
