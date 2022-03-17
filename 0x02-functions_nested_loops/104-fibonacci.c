0ef4f9cacf1bc75d6508aab83dd2bc9097670e6a
root <abenezerlulseged@gmail.com>
2022-03-17 11:41:44 -0700

    nested functions

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,32 +1,50 @@
 #include <stdio.h>
 
 /**
- * main - prints the sum of all even Fibonacci numbers below 4,000,000
+ * main - Prints the first 98 Fibonacci numbers
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-	long int total_sum, sum, first, second;
+int c, boolean, boolean2;
+long int n1, n2, fn, fn2, n11, n22;
 
-	total_sum = 0;
-	sum = 0;
-	first = 0;
-	second = 1;
-
-	while (sum < 4000000)
-	{
-		sum = first + second;
-		if (sum % 2 == 0)
-		{
-			total_sum += sum;
-		}
-		first = second;
-		second = sum;
-	}
-
-	printf("%li\n", total_sum);
-
-	return (0);
+n1 = 1;
+n2 = 2;
+boolean =  boolean2 = 1;
+printf("%ld, %ld", n1, n2);
+for (c = 0; c < 96; c++)
+{
+if (boolean)
+{
+fn = n1 + n2;
+printf(", %ld", fn);
+n1 = n2;
+n2 = fn;
+}
+else
+{
+if (boolean2)
+{
+n11 = n1 % 1000000000;
+n22 = n2 % 1000000000;
+n1 = n1 / 1000000000;
+n2 = n2 / 1000000000;
+boolean2 = 0;
+}
+fn2 = (n11 + n22);
+fn = n1 + n2 + (fn2 / 1000000000);
+printf(", %ld", fn);
+printf("%ld", fn2 % 1000000000);
+n1 = n2;
+n11 = n22;
+n2 = fn;
+n22 = (fn2 % 1000000000);
+}
+if (((n1 + n2) < 0) && boolean == 1)
+boolean = 0;
+}
+printf("\n");
+return (0);
 }
